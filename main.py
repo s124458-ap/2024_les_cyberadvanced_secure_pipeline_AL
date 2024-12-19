0e24ad3d6a17f7a5794ee15027bd7b4d006c3bae
s124458-ap <s124458-@ap.be>
2024-12-19 16:25:37 +0100

    edit test workflow

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import os
+import subprocess
+import sys
 print("Hello World!")
-password="ditmagjenietweten"
-os.system("echo " + password + " | sudo -S touch safe.txt")
+#password="ditmagjenietweten"
+#os.system("echo " + password + " | sudo -S touch safe.txt")
+
+def commando_toevoegen():
+    subprocess.call("touch" + sys.argv[1], shell =True)
+
+commando_toevoegen()
